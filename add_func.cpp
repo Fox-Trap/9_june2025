0feed65739fc32789fbbda94152cf2a2362c4e15
KARAN KUMAR JHA <73754506+Fox-Trap@users.noreply.github.com>
2025-06-13 19:52:33 +0530

    Add files via upload

---
+++ add_func.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function definition
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int x = 5, y = 3;
+    int result = add(x, y);  // Function call
+    cout << "Sum: " << result << endl;
+    return 0;
+}
